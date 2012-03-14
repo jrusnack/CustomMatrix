7a3d64842e0f5adba274042483942a396e56d791
Jan Rusnacko <jrusnack@redhat.com>
2012-03-14 14:35:16 +0100

    Finalizing

--- src/main/java/com/redhat/engineering/jenkins/custom/matrix/Definitions.java
+++ src/main/java/com/redhat/engineering/jenkins/custom/matrix/Definitions.java
@@ -34,6 +34,6 @@ public abstract class Definitions
 	public static final String __REBUILD_VAR_NAME = "REBUILD";
 	
 	public static final String __DISPLAY_NAME   = "Custom Matrix";
-	public static final String __ICON_FILE_NAME = "/plugin/matrix-reloaded/images/matrix_small.png";
+	public static final String __ICON_FILE_NAME = "/plugin/custom-matrix/images/custom-matrix_small.png";
 	public static final String __URL_NAME       = "custom-matrix";
 }
